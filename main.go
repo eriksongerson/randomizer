47d5540007c98d4bc2dd8f734bc001cf78edd767
eriksongerson <eriksongerson@yandex.ru>
2018-05-13 16:07:28 +0300

    carriage and rand fix

--- main.go
+++ main.go
@@ -4,9 +4,12 @@ import (
 	"fmt"
 	"math/rand"
 	"os"
+	"time"
 )
 
 func main() {
+	rand.Seed(time.Now().UnixNano())
+
 	f,err:= os.Create("task.txt")
 	if(err != nil){
 		return
@@ -14,17 +17,17 @@ func main() {
 	defer f.Close()
 
 	fmt.Println("Tasks for today:")
-	f.WriteString("Tasks for today:\n")
+	f.WriteString("Tasks for today:\r\n")
 	for i := 0; i < 50; i++ {
 		num:= rand.Uint32()
 		fmt.Println(num)
-		str := fmt.Sprint(num)
-		f.WriteString(str + "\n")
+		str := fmt.Sprint(num) + "\r\n"
+		f.WriteString(str)
 	}
 	for i := 0; i < 50; i++ {
 		num:= rand.Uint64()
 		fmt.Println(num)
 		str := fmt.Sprint(num)
-		f.WriteString(str + "\n")
+		f.WriteString(str + "\r\n")
 	}
 }
